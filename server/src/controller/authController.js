1dd30ef258f687f6fa7019f3a471eb7517665633
marc <marccastillo621@gmail.com>
2025-04-07 00:57:24 +0800

    added user signup

--- server/src/controller/authController.js
+++ server/src/controller/authController.js
@@ -2,10 +2,50 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+//generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+};
+
 // Register a new user
 export const register = async (req, res) => {
+  const { username, password, email, profileImage } = req.body;
   try {
-    res.status(200).json({ message: "Register endpoint hit" });
+    if (!username || !password || !email) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const userExists = await User.findOne({
+      email,
+    });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      email,
+      profileImage,
+    });
+
+    await newUser.save();
+
+    const token = generateToken(newUser._id);
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      profileImage: newUser.profileImage,
+      token,
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
